7434eb656109cdda5316cd77f7e4d2916bda0cb6
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-09-05 03:15:36 +0200

    up register page

--- src/components/Login/RegisterForm.js
+++ src/components/Login/RegisterForm.js
@@ -4,7 +4,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 //bootstrap components
 import Form from 'react-bootstrap/Form';
-import * as yup from 'yup';
+// import * as yup from 'yup';
 //api request
 import { public_request } from '../../util/requestMethods'
 //functions 
@@ -18,53 +18,66 @@ const initialValue = {
     // confirm_password: ''
 }
 function RegisterForm() {
-    const [formErrors, setFormErrors] = useState(initialValue);
+    const [formErrors, setFormErrors] = useState('');
     const [formValues, setFormValues] = useState(initialValue);
-    const [disable, setDisable] = useState(true);
+    const [disable, setDisable] = useState(false);
 
     ///get values 
-    const handleData = (e) => {
+    const handleData = async (e) => {
         const { name, value } = e.target;
-        yup.reach(registerFormSchema, name)
-            .validate(value)
-            .then(() => {
-                setFormErrors({ ...formErrors, [name]: '' });
-                setDisable(false);
-            })
-            .catch((err) => {
-                setFormErrors({ ...formErrors, [name]: err.errors })
-            })
-        setFormValues({ ...formValues, [name]: value })
+        setFormValues({ ...formValues, [name]: value });
+        // some errors here
+        // await yup.reach(registerFormSchema, name)
+        //     .validate(value)
+        //     .then((valid) => {
+        //         setFormErrors({ ...formErrors, [name]: '' });
+        //         if (valid.errors.length === 0) {
+        //             setDisable(false);
+        //         }
+        //     }).catch((err) => {
+        //         setDisable(true);
+        //         setFormErrors({ ...formErrors, [name]: err.errors })
+        //     })
+
     }
 
     //submit form
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await public_request.post('users/auth/create', { ...formValues })
-            .then(response => {
-                toast.success(response?.data?.message, {
-                    position: "top-right",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                });
-                setFormValues(initialValue);
-                setDisable(true);
+        registerFormSchema.validate(formValues)
+            .catch((err) => {
+                setFormErrors(err.message);
+            }).then(async (valid) => {
+                if (valid) {
+                    setFormErrors('');
+                    await public_request.post('users/auth/create', { ...formValues })
+                        .then(response => {
+                            toast.success(response?.data?.message, {
+                                position: "top-right",
+                                autoClose: 2000,
+                                hideProgressBar: false,
+                                closeOnClick: true,
+                                pauseOnHover: true,
+                                draggable: true,
+                                progress: undefined,
+                            });
+                            setFormValues(initialValue);
+                            setDisable(true);
+                        })
+                        .catch(err => {
+                            toast.error(err?.response?.data?.message, {
+                                position: "top-right",
+                                autoClose: 2000,
+                                hideProgressBar: false,
+                                closeOnClick: true,
+                                pauseOnHover: true,
+                                draggable: true,
+                                progress: undefined,
+                            });
+                        })
+                }
             })
-            .catch(err => {
-                toast.error(err?.response?.data?.message, {
-                    position: "top-right",
-                    autoClose: 2000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                });
-            })
+
     }
     return (
         <Form>
@@ -74,25 +87,27 @@ function RegisterForm() {
                 experience throughout this website,
                 to manage access to your account.
             </Form.Text>
+            <br />
+            <p className="text-danger">{formErrors}</p>
             <Form.Group className="mb-3" controlId="formBasicUsernameR">
                 <Form.Control type="text" placeholder="username" name="name" className='rounded-5' value={formValues.name} onChange={handleData} />
-                <Form.Label className="text-danger">{formErrors.name}</Form.Label>
+                {/* <Form.Label className="text-danger">{formErrors.name}</Form.Label> */}
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmailR">
                 <Form.Control type="email" placeholder="Enter email" name="email" className='rounded-5' value={formValues.email} onChange={handleData} />
-                <Form.Label className="text-danger">{formErrors.email}</Form.Label>
+                {/* <Form.Label className="text-danger">{formErrors.email}</Form.Label> */}
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPasswordR">
                 <Form.Control type="password" placeholder="Password" name="password" className='rounded-5' value={formValues.password} onChange={handleData} />
-                <Form.Label className="text-danger">{formErrors.password}</Form.Label>
+                {/* <Form.Label className="text-danger">{formErrors.password}</Form.Label> */}
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicConfirmR">
                 <Form.Control type="text" placeholder="number" name="number" className='rounded-5' value={formValues.number} onChange={handleData} />
-                <Form.Label className="text-danger">{formErrors.number}</Form.Label>
+                {/* <Form.Label className="text-danger">{formErrors.number}</Form.Label> */}
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicConfirmR">
                 <Form.Control type="text" placeholder="address" name="address" className='rounded-5' value={formValues.address} onChange={handleData} />
-                <Form.Label className="text-danger">{formErrors.address}</Form.Label>
+                {/* <Form.Label className="text-danger">{formErrors.address}</Form.Label> */}
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="I agree to terms and Policy." />
